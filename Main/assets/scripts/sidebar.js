57312de31630027e2f5edabb43ac23fba1e7f59d
bitcamp\BIT <0215bitcamp@gmail.com>
2024-09-23 14:56:00 +0900

    2024-09-23 1.사이드바 커버 수정

--- Main/assets/scripts/sidebar.js
+++ Main/assets/scripts/sidebar.js
@@ -5,12 +5,12 @@ const main = document.querySelector('.main');
 // 사이드바 열고 닫기 함수
 function openSidebar() {
     extraSidebar.style.left = '4.375rem'; // sidebar의 너비만큼 이동
-    main.style.marginLeft = '15rem'; // extra-sidebar와 함께 main도 이동
+    // main.style.marginLeft = '15rem'; // extra-sidebar와 함께 main도 이동
 }
 
 function closeSidebar() {
     extraSidebar.style.left = '-10.625rem'; // extra-sidebar를 숨김
-    main.style.marginLeft = '4.375rem'; // main이 sidebar로 다시 붙음
+    // main.style.marginLeft = '4.375rem'; // main이 sidebar로 다시 붙음
 }
 
 // sidebar에 마우스를 올렸을 때
