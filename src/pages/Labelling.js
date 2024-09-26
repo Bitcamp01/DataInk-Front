68244e99426c7a697969ab4de4ac14a8c3a8e84d
bit\bitcamp <byungju98@gmail.com>
2024-09-26 19:34:35 +0900

    Feat: 0926 김병주 라벨링 페이지 거의 완성
    1. 보이는 부분은 거의 동일하게 완성(검색바 변경, 사이드바 추가, 테이블 빌려씀)
    2. 사이드바 접었다 폈다 미구현
    3. 테이블 화영님거 빌려오기만 한거라 이래저래 미완성

--- src/pages/Labelling.js
+++ src/pages/Labelling.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import SearchComponent from '../components/SearchComponent'
+import SearchComponent from '../components/labelling/SearchComponent'
+import CustomTreeView from '../components/labelling/TreeView'
+import DataGridDemo from '../components/Table'
+import { Grid } from '@mui/material'
 
 const labelling = () => {
   return (
-    <div>
+    <Grid container>
       <SearchComponent/>
-    </div>
+      <Grid container>
+        <Grid item xs={1}>
+          <CustomTreeView/>
+        </Grid>
+
+        <Grid item xs={1.5}>
+        </Grid> 
+
+        <Grid item flex={1} sx={{ marginTop: '8.625rem', marginRight: '3rem' }}>
+          <DataGridDemo />
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
