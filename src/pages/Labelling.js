868344ffd2e89181cb549dfc025f1a9d9a48e0e9
bit\bitcamp <byungju98@gmail.com>
2024-09-26 22:02:41 +0900

    Modify: 0926 김병주 라벨링 테이블 동적으로 변경

--- src/pages/Labelling.js
+++ src/pages/Labelling.js
@@ -1,27 +1,43 @@
-import React from 'react'
-import SearchComponent from '../components/labelling/SearchComponent'
-import CustomTreeView from '../components/labelling/TreeView'
-import DataGridDemo from '../components/Table'
-import { Grid } from '@mui/material'
+import React from 'react';
+import SearchComponent from '../components/labelling/SearchComponent';
+import CustomTreeView from '../components/labelling/TreeView';
+import DataGridDemo from '../components/Table_label';
+import { Box } from '@mui/material';
 
-const labelling = () => {
+const Labelling = () => {
   return (
-    <Grid container>
-      <SearchComponent/>
-      <Grid container>
-        <Grid item xs={1}>
-          <CustomTreeView/>
-        </Grid>
+    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+      
+      {/* SearchComponent - 상단에 위치 */}
+      <Box sx={{ flexShrink: 0 }}>
+        <SearchComponent />
+      </Box>
 
-        <Grid item xs={1.5}>
-        </Grid> 
+      {/* 사이드바와 테이블을 배치하는 영역 */}
+      <Box sx={{ display: 'flex', flexGrow: 1, overflow: 'hidden' }}>
+        
+        {/* 사이드바 영역 - 왼쪽에 고정 */}
+        <Box sx={{ width: '250px', minWidth: '200px', backgroundColor: '#f0f0f0', overflowY: 'auto' }}>
+          <CustomTreeView />
+        </Box>
 
-        <Grid item flex={1} sx={{ marginTop: '8.625rem', marginRight: '3rem' }}>
-          <DataGridDemo />
-        </Grid>
-      </Grid>
-    </Grid>
-  )
-}
+        {/* 테이블 영역 - 중앙 정렬 및 좌우 스크롤 활성화 */}
+        <Box sx={{ 
+          flexGrow: 1, 
+          marginLeft: '2rem', 
+          display: 'flex', 
+          justifyContent: 'center',  // 수평 가운데 정렬
+          alignItems: 'center',      // 수직 가운데 정렬
+          overflowX: 'auto',         // 좌우 스크롤 가능하게 설정
+          overflowY: 'auto',         // 필요시 상하 스크롤도 가능하게 설정
+        }}>
+          <Box sx={{ minWidth: '100%' }}> {/* 테이블 크기 설정으로 스크롤 유도 */}
+            <DataGridDemo />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
-export default labelling
+export default Labelling;
