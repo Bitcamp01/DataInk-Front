27fe9b8f142679e1d70053712d04a9dac10b3149
bit\bitcamp <byungju98@gmail.com>
2024-09-30 10:22:36 +0900

    Add: 0930 김병주 사이드바 접고 펴기 추가(디테일 개선 필요)

--- React/src/components/labelling/TreeView.jsx
+++ React/src/components/labelling/TreeView.jsx
@@ -4,6 +4,12 @@ import '../../css/tree-view.css';
 
 const TreeView = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [isVisible, setIsVisible] = useState(true);
+
+  // 버튼 클릭 시 토글
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
 
   const ITEMS = [
     {
@@ -50,19 +56,31 @@ const TreeView = () => {
   };
 
   return (
-    <div className="tree-container">
-      <div className="search-bar">
-        <img src={`/icons/label-search_icon.svg`} alt="Label Search Icon" className="label-search-icon" />
-        <input 
-          type="text" 
-          placeholder="Search" 
-          value={searchTerm} 
-          onChange={handleSearch} 
-          className="search-input"
-        />
-      </div>
-      <RichTreeView items={ITEMS} />
-      <button className="side-button">
+    <div
+      className="tree-container"
+      style={{
+        width: isVisible ? '15.6rem' : '0', // isVisible에 따라 width 제어
+      }}
+    >
+      {/* search-bar와 RichTreeView가 isVisible 값에 따라 렌더링 */}
+      {isVisible && (
+        <>
+          <div className="search-bar">
+            <img src={`/icons/label-search_icon.svg`} alt="Label Search Icon" className="label-search-icon" />
+            <input 
+              type="text" 
+              placeholder="Search" 
+              value={searchTerm} 
+              onChange={handleSearch} 
+              className="search-input"
+            />
+          </div>
+          <RichTreeView items={ITEMS} />
+        </>
+      )}
+
+      {/* 버튼을 누르면 가시성을 토글 */}
+      <button className="side-button" onClick={toggleVisibility}>
         <img src="/icons/back_icon.svg" alt="Side Button" />
       </button>
     </div>
