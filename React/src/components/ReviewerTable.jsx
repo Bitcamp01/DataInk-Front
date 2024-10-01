79dd19baaac7bd6ff598e8b725399a7dc24c8d95
Taelim Ma <tlma001003@gmail.com>
2024-10-01 18:33:24 +0900

    Modify 1001 마태림 검수페이지 테이블 완성 / 데이터를 받았을 때 테이블 확장 및 축소 기능 편리화는 아직입니다

--- React/src/components/ReviewerTable.jsx
+++ React/src/components/ReviewerTable.jsx
@@ -1,17 +1,77 @@
 import React from 'react';
+import '../css/reviewer.css';
 
 const ReviewerTable = () => {
     return (
         <div className="review-table-container">
             <table className="review-table">
-                {[...Array(24)].map((_, index) => (
-                    <tr key={index}>
-                        <td className="review-row-item">Row name <span></span></td>
+                <thead>
+                    <tr class="review-table-header">
+                        <th colspan="5">분류명</th>
+                        <th>내용</th>
                     </tr>
-                ))}
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan="5" class="review-divide">사건명</td>
+                        <td class="review-detail"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="5" class="review-divide">사건번호</td>
+                        <td class="review-detail"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="5" class="review-divide">제출일자</td>
+                        <td class="review-detail"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" rowspan="3" class="review-divide">이유</td>
+                        <td colspan="3" class="review-divide">이유1</td>
+                        <td class="review-detail"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="3" class="review-divide">이유2</td>
+                        <td class="review-detail"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="3" class="review-divide">이유3</td>
+                        <td class="review-detail"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" rowspan="2" class="review-divide">사건명</td>
+                        <td colspan="3" class="review-divide">한글</td>
+                        <td class="review-detail"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="3" class="review-divide">영어</td>
+                        <td class="review-detail"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" rowspan="4" class="review-divide">참조</td>
+                        <td colspan="2" rowspan="2" class="review-divide">참조1</td>
+                        <td class="review-divide">참조1-1</td>
+                        <td class="review-detail"></td>
+                    </tr>
+                    <tr>
+                        <td class="review-divide">참조1-2</td>
+                        <td class="review-detail"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" rowspan="2" class="review-divide">참조2</td>
+                        <td class="review-divide">참조2-1</td>
+                        <td class="review-detail"></td>
+                    </tr>
+                    <tr>
+                        <td class="review-divide">참조2-2</td>
+                        <td class="review-detail"></td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     );
 };
 
 export default ReviewerTable;
+
+
+// 실제로 데이터가 들어갈 때는 rowspan colspan을 이용해서 테이블을 조절해야 할 것 같다.
