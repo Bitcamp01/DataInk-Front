b99706914976e3463f44378abc7c6659c9f96c2d
WIN-HR35D92QS4P\soominchoi <csm0062@naver.com>
2024-10-04 23:29:44 +0900

    Modify: axios 주석처리

--- React/src/pages/ProfileInit.js
+++ React/src/pages/ProfileInit.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import '../css/profileInit.css';
-import axios from 'axios';
+// import axios from 'axios';
 
 const ProfileInit = ({onAuthenticate}) => {
     const navigate = useNavigate();
