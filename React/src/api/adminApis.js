f332d37d851c35e56ee1c10991ee7d24eddb3a28
bitcamp\BIT <0215bitcamp@gmail.com>
2024-10-07 11:43:32 +0900

    ADD 파일api수정

--- React/src/api/adminApis.js
+++ React/src/api/adminApis.js
@@ -1,164 +1,92 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 import config from './../config.js';
+import{createAsyncThunk} from '@reduxjs/toolkit';
+// import axios from 'axios';
 
-// 파일 목록 가져오기
-export const listFiles = createAsyncThunk(
-    'fileManager/listFiles',
-    async (path, { rejectWithValue }) => {
-        try {
-            const response = await axios.get(config.url_list, {
-                params: {
-                    path: encodeURIComponent(path) || '/'
-                }
-            });
-            return response.data;
-        } catch (error) {
-            return rejectWithValue(error.response.data);
+export function list(path) {
+    return fetch(config.url_list + '?path=' + (encodeURIComponent(path) || '/'));
+
+};
+
+export function createDirectory(path, directory) {
+    return fetch(config.url_create_folder, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            path, directory
+        })
+    });
+};
+
+
+export function getFileContent(path) {
+    return fetch(config.url_get_content + '?path=' + (encodeURIComponent(path) || '/'));
+};
+
+
+
+export function remove(path, filenames, recursive = true) {
+    return fetch(config.url_remove, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            path, filenames, recursive
+        })
+    });
+};
+
+export function move(path, destination, filenames) {
+    return fetch(config.url_move, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            path, destination, filenames
+        })
+    });
+};
+
+export function rename(path, destination) {
+    return fetch(config.url_rename, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            path, destination
+        })
+    });
+};
+
+export function copy(path, destination, filenames) {
+    return fetch(config.url_copy, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            path, destination, filenames
+        })
+    });
+};
+
+
+export function upload(path, fileList, formData = new FormData()) {
+    [...fileList].forEach(f => {
+        formData.append('file[]', f);
+    });
+    formData.append('path', path);
+
+    return fetch(config.url_upload, {
+        method: 'POST',
+        body: formData,
+        headers: {
+            // a workaround for node connector, passing the path by header
+            path: path
         }
-    }
-);
-
-// 디렉토리 생성
-export const createDirectory = createAsyncThunk(
-    'fileManager/createDirectory',
-    async ({ path, directory }, { rejectWithValue }) => {
-        try {
-            const response = await axios.post(config.url_create_folder, {
-                path,
-                directory
-            }, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            return response.data;
-        } catch (error) {
-            return rejectWithValue(error.response.data);
-        }
-    }
-);
-
-// 파일 내용 가져오기
-export const getFileContent = createAsyncThunk(
-    'fileManager/getFileContent',
-    async (path, { rejectWithValue }) => {
-        try {
-            const response = await axios.get(config.url_get_content, {
-                params: {
-                    path: encodeURIComponent(path) || '/'
-                }
-            });
-            return response.data;
-        } catch (error) {
-            return rejectWithValue(error.response.data);
-        }
-    }
-);
-
-// 파일 또는 폴더 삭제
-export const removeFiles = createAsyncThunk(
-    'fileManager/removeFiles',
-    async ({ path, filenames, recursive = true }, { rejectWithValue }) => {
-        try {
-            const response = await axios.post(config.url_remove, {
-                path,
-                filenames,
-                recursive
-            }, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            return response.data;
-        } catch (error) {
-            return rejectWithValue(error.response.data);
-        }
-    }
-);
-
-// 파일 이동
-export const moveFiles = createAsyncThunk(
-    'fileManager/moveFiles',
-    async ({ path, destination, filenames }, { rejectWithValue }) => {
-        try {
-            const response = await axios.post(config.url_move, {
-                path,
-                destination,
-                filenames
-            }, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            return response.data;
-        } catch (error) {
-            return rejectWithValue(error.response.data);
-        }
-    }
-);
-
-// 파일 이름 변경
-export const renameFile = createAsyncThunk(
-    'fileManager/renameFile',
-    async ({ path, newPath }, { rejectWithValue }) => {
-        try {
-            const response = await axios.post(config.url_rename, {
-                path,
-                destination: newPath
-            }, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            return response.data;
-        } catch (error) {
-            return rejectWithValue(error.response.data);
-        }
-    }
-);
-
-// 파일 복사
-export const copyFiles = createAsyncThunk(
-    'fileManager/copyFiles',
-    async ({ path, destination, filenames }, { rejectWithValue }) => {
-        try {
-            const response = await axios.post(config.url_copy, {
-                path,
-                destination,
-                filenames
-            }, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            return response.data;
-        } catch (error) {
-            return rejectWithValue(error.response.data);
-        }
-    }
-);
-
-// 파일 업로드
-export const uploadFiles = createAsyncThunk(
-    'fileManager/uploadFiles',
-    async ({ path, fileList }, { rejectWithValue }) => {
-        try {
-            const formData = new FormData();
-            [...fileList].forEach(f => {
-                formData.append('file[]', f);
-            });
-            formData.append('path', path);
-
-            const response = await axios.post(config.url_upload, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                    path: path
-                }
-            });
-            return response.data;
-        } catch (error) {
-            return rejectWithValue(error.response.data);
-        }
-    }
-);
+    });
+};
