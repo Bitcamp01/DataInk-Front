53972cf6f4c72120182a304d5952d6fee62ac4b3
zaiha <45660311+215woghks@users.noreply.github.com>
2024-10-08 22:02:13 +0900

    MOD 코드 문제 있어서 잠시 내립니다.

--- React/src/index.js
+++ React/src/index.js
@@ -4,18 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
-import {configureStore} from "@reduxjs/toolkit";
-import AdminFileReducer from "./reducers/AdminFileReducer";
-import {Provider} from "react-redux";
-const store = configureStore({
-    reducer: AdminFileReducer
-});
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <App />
     </BrowserRouter>
 );
 
--- React/src/pages/ProjectStructure.js
+++ React/src/pages/ProjectStructure.js
@@ -1,68 +1,27 @@
-import React, { Component } from 'react';
-import FileList from '../components/projectStructure/adminProjectStructureFileList/FileList.jsx';
-import Navbar from '../components/projectStructure/adminProjectStructureNavbar/Navbar.jsx';
-import ContextMenu from '../components/projectStructure/adminProjectStructureContextMenu/ContextMenu.jsx';
-import Dialogs from '../components/projectStructure/adminProjectStructureDialogs/Dialogs.jsx';
+import React from 'react';
+import PdfViewer from '../components/PdfViewer'; // PDF 뷰어 컴포넌트
+import SelectForm from '../components/SelectForm'; // SelectForm 컴포넌트
+import ReviewerTable from '../components/ReviewerTable'; // ReviewerTable 컴포넌트
+import '../css/reviewer.css';
 
+const ProjectStructure = () => {
+    // PDF 파일 경로를 여기에 설정
+    const pdfFilePath = '/practice.pdf'; // 상대 경로 설정
 
-import {ThemeProvider as MaterialUI, createTheme, Button} from '@mui/material';
-import { blue } from '@mui/material/colors';
-import {connect} from 'react-redux';
-import {setContextMenuVisible, refreshFileList, goToParentDirectory} from '../actions/Actions.js';
-import DynamicSnackbar from '../components/projectStructure/adminProjectStructureNotification/DynamicSnackbar.jsx';
-import TreeView from "../components/projectStructure/adminProjectStructureTreeView/TreeView";
-
-const theme = createTheme({
-    palette: {
-        primary: blue,
-    }
-});
-
-class AdministratorProjectStructure extends Component {
-
-    componentDidMount() {
-        this.props.init();
-    };
-
-    render() {
-        return (
-                <MaterialUI theme={theme}>
-                    <div onClick={this.props.handleHideContextMenu} onContextMenu={this.props.handleHideContextMenu}>
-                        <Navbar />
-                        <Button variant="outlined" onClick={this.props.goToParentDirectory}>
-                            상위 폴더로 이동
-                        </Button>
-                        {/*<TreeView/>*/}
-                        <FileList />
-                        <ContextMenu />
-                        <DynamicSnackbar />
-                        <Dialogs />
-                    </div>
-                </MaterialUI>
-        );
-    }
-}
-
-const mapStateToProps = (state) => {
-    return {
-    };
+    return (
+        <section className="review-content">
+            <div className="pdf-section">
+                {/* fileUrl prop을 PdfViewer에 전달 */}
+                <PdfViewer fileUrl={pdfFilePath} />
+            </div>
+            <div className="work-space">
+                <div className="review-work-container">
+                    <ReviewerTable />
+                    <SelectForm />
+                </div>
+            </div>
+        </section>
+    );
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        init: () => {
-            dispatch(refreshFileList());
-        },
-        goToParentDirectory: () => {
-            dispatch(goToParentDirectory());  // 상위 폴더 이동 액션 디스패치
-        },
-        handleHideContextMenu: (event) => {
-            if (! (event.target.tagName === 'INPUT' || /label/i.test(event.target.className))) {
-                event.preventDefault();
-            }
-            dispatch(setContextMenuVisible(false));
-        }
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(AdministratorProjectStructure);
+export default ProjectStructure;
