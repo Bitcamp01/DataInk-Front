bd2535d8d4daf1017e61e0c74e2c0ea198577d12
bit\bitcamp <byungju98@gmail.com>
2024-10-10 21:34:49 +0900

    Add: 1010 김병주 redux-persist 의존성 추가 및 적용

--- React/src/App.js
+++ React/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Dashboard from './pages/Dashboard';
 import Join from './pages/Join';
@@ -15,32 +15,45 @@ import Notice_write from './pages/Notice_write';
 import Notice_detail from './pages/Notice_detail';
 import FindAccount from './pages/FindAccount';
 import LabellingDetail from './pages/LabellingDetail';
-import ItemStructure from "./pages/ItemStructure";
-import ProjectStructure from './pages/ProjectStructure';
-import MainGrid from "./pages/MainGrid";
+import MainGrid from './pages/MainGrid';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import PrivateRoute from './components/PrivateRoute';
+
 function App() {
+  const persiststore = persistStore(store);
+
   return (
-    <Routes>
-      {/* Layout이 모든 페이지에 공통으로 적용될 구조 */}
-      <Route element={<Layout />}>
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/label/main' element={<LabellingMain />} />
-        <Route path='/label/work' element={<Labelling />} />
-        <Route path='/label/detail' element={<LabellingDetail />} />
-        <Route path='/member' element={<MemberManagement />} />
-        <Route path='/mypage' element={<Mypage />} />
-        <Route path='/notice' element={<Notice />} />
-        <Route path='/review' element={<Review />} />
-        <Route path="/notice_write" element={<Notice_write />} />
-        <Route path="/notice_detail" element={<Notice_detail />} />
-          <Route path="/main_grid" element={<MainGrid />} />
-      </Route>
-      <Route>
-        <Route path='/join' element={<Join />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/findaccount' element={<FindAccount />} />
-      </Route>
-    </Routes>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persiststore}>
+        <Routes>
+          {/* 기본 경로에서 로그인 페이지로 리다이렉트 */}
+          <Route path="/" element={<Navigate to="/login" />} />
+
+          {/* Private Routes */}
+          <Route path="/" element={<PrivateRoute><Layout /></PrivateRoute>}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/label/main" element={<LabellingMain />} />
+            <Route path="/label/work" element={<Labelling />} />
+            <Route path="/label/detail" element={<LabellingDetail />} />
+            <Route path="/member" element={<MemberManagement />} />
+            <Route path="/mypage" element={<Mypage />} />
+            <Route path="/notice" element={<Notice />} />
+            <Route path="/review" element={<Review />} />
+            <Route path="/notice_write" element={<Notice_write />} />
+            <Route path="/notice_detail" element={<Notice_detail />} />
+            <Route path="/main_grid" element={<MainGrid />} />
+          </Route>
+
+          {/* Public Routes */}
+          <Route path="/join" element={<Join />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/findaccount" element={<FindAccount />} />
+        </Routes>
+      </PersistGate>
+    </Provider>
   );
 }
 
