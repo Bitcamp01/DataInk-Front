a013d523ad89c05653f341ba406532a864220f8c
bit\bitcamp <byungju98@gmail.com>
2024-10-11 14:18:41 +0900

    Modify: 1011 김병주 공통부분 헤더 페이지 제목 나오게 수정

--- React/src/pages/Layout.js
+++ React/src/pages/Layout.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import '../css/main.css';
 import Header from '../components/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 function Layout() {
+  const location = useLocation();
+  const [pageTitle, setPageTitle] = useState('Data Labeling');
+
+  // 페이지 제목을 설정하는 함수
+  const getPageTitle = (path) => {
+    switch (path) {
+      case '/dashboard':
+        return 'Dashboard';
+      case '/label/main':
+        return 'Labelling Select';
+      case '/label/work':
+        return 'Labelling Work';
+      case '/label/detail':
+        return 'Labelling Detail';
+      case '/member':
+        return 'Member Management';
+      case '/mypage':
+        return 'MyPage';
+      case '/notice':
+        return 'Notice';
+      case '/review':
+        return 'Labelling Review';
+      case '/notice_write':
+        return 'Notice';
+      case '/notice_detail':
+        return 'Notice';
+      default:
+        return 'Data Labeling'; // 기본 제목
+    }
+  };
+
+  useEffect(() => {
+    const title = getPageTitle(location.pathname);
+    setPageTitle(title);
+  }, [location]);
+
+
   return (
     <div className="layout">
       {/* 사이드바 */}
@@ -13,7 +50,7 @@ function Layout() {
       {/* 메인 영역 */}
       <div className="main">
         {/* 헤더 */}
-        <Header />
+        <Header title={pageTitle} />
         
         {/* Outlet을 사용해 각 페이지의 콘텐츠 렌더링 */}
         <Outlet />
