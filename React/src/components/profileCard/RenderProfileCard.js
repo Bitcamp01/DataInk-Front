ead0ea50a19215ec19d588fb50f80dea8339f8cd
bit\bitcamp <havebeen4@naver.com>
2024-10-24 15:41:49 +0900

    Modify: 1024 오유빈 충돌나는 CSS와 html 클래스 이름 변경 및 수정 완료

--- React/src/components/profileCard/RenderProfileCard.js
+++ React/src/components/profileCard/RenderProfileCard.js
@@ -9,7 +9,7 @@ const RenderProfileCard = () => {
     const fetchedProfile = async () => {
       const token = sessionStorage.getItem('ACCESS_TOKEN');
       try {
-        const response = await axios.get('http://localhost:9090/api/profile', {
+        const response = await axios.get('http://localhost:9090/profile/profiles', {
           headers: {
             Authorization: `Bearer ${token}`
           },
@@ -37,7 +37,7 @@ const RenderProfileCard = () => {
     const fetchedNotice = async () => {
       const token = sessionStorage.getItem('ACCESS_TOKEN');
       try {
-        const response = await axios.get('http://localhost:9090/api/notices', {
+        const response = await axios.get('http://localhost:9090/profile/notices', {
           headers: {
             Authorization: `Bearer ${token}`
           },
