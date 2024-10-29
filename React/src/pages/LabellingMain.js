91f2dd31521d6656106e1be659040d08bcd6c4ec
bit\bitcamp <byungju98@gmail.com>
2024-10-29 21:27:27 +0900

    Modify: 1029 김병주 라벨링 선택 페이지 버튼 관리자만, 프로젝트수 백엔드

--- React/src/pages/LabellingMain.js
+++ React/src/pages/LabellingMain.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import RendarProjectCard from '../components/projectCard/RendarProjectCard'
 import CommonButton from '../components/CommonButton';
 import {useNavigate} from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 const ProjectContainer = styled.div`
     margin-left: 3.3rem;
@@ -30,17 +31,26 @@ const NewProjectButton = styled(CommonButton)`
 
 const Labelling = () => {
     const navi=useNavigate();
+    const authen = useSelector((state) => state.userSlice.authen); // authen 값을 가져옴
+    const projectCount = useSelector((state) => {
+      const projects = state.userProjectsSlice.projects || []; // 기본값을 빈 배열로 설정
+      const currentDate = new Date(); // 현재 날짜
+      return projects.filter(project => new Date(project.endDate) >= currentDate).length; // 종료일이 현재 날짜 이후인 프로젝트만 필터링
+    });
+
     const handleNewProject = () => {
         navi('/main_grid');
     };
   return (
     <ProjectContainer>
         <h2 className='project-header'>
-            참여 중인 프로젝트 <span className='project-count'>10</span><span>건</span>
+          참여 중인 프로젝트&nbsp;<span className='project-count'>{projectCount}</span><span>건</span>
         </h2>
-        <ButtonContainer>
-            <NewProjectButton onClick={handleNewProject}>프로젝트 추가</NewProjectButton>
-        </ButtonContainer>
+        {authen === 'ROLE_ADMIN' && ( // authen이 ROLE_ADMIN일 때만 렌더링
+                <ButtonContainer>
+                    <NewProjectButton onClick={handleNewProject}>프로젝트 추가</NewProjectButton>
+                </ButtonContainer>
+            )}
         <ParentContainer>
             <RendarProjectCard />
         </ParentContainer>
